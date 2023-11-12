37480a37ae20daa6a1600c5fe26b424c697e15c3
Olga Fomenkova <olga.fomenkova@pprcard.ru>
2023-11-12 14:10:15 +0300

    New texts for the 3rd stage

--- Learning Progress Tracker (Java)/task/src/tracker/Messages.java
+++ Learning Progress Tracker (Java)/task/src/tracker/Messages.java
@@ -5,11 +5,21 @@ public enum Messages {
     EXIT_SUGGESTION("Enter 'exit' to exit the program."),
     EXIT("Bye!\n"),
     NO_INPUT("No input.\n"),
+    NO_STUDENTS("No students found\n"),
     COMMAND_ERROR("Unknown command!\n"),
     ADD_ACTION("Enter student credentials or 'back' to return:\n"),
     ADD_SUCCESSFUL("The student has been added.\n"),
     ADD_COMPLETE("Total %d students have been added.\n"),
-    ADD_ERROR("Incorrect %s.\n");
+    ADD_PONTS("Enter an id and points or 'back' to return\n"),
+    ADD_ERROR("Incorrect %s.\n"),
+    EMAIL_EXISTS("This email is already taken.\n"),
+    INCORRECT_STUDENT_ID("No student is found for id=%s\n"),
+    INCORRECT_POINTS_FORMAT("Incorrect points format\n"),
+    POINTS_UPDATED("Points updated\n"),
+    FIND_STUDENTS("Enter an id or 'back' to return"),
+    STUDENT_DATA("id points: Java=%d; DSA=%d; Databases=%d; Spring=%d\n");
+
+
     private final String message;
 
     Messages(String message) {
