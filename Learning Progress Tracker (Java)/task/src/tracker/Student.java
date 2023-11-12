eb8222e4a6f9b66d0e469a779adcde8db17195be
Olga Fomenkova <olga.fomenkova@pprcard.ru>
2023-11-12 15:06:46 +0300

    Check emails and add students

--- Learning Progress Tracker (Java)/task/src/tracker/Student.java
+++ Learning Progress Tracker (Java)/task/src/tracker/Student.java
@@ -1,11 +1,22 @@
 package tracker;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Student {
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    static Map<String,String> students = new LinkedHashMap<>();
 
     public static boolean checkCredentials(String data) {
         if (data.chars().filter(c -> c == (int) ' ').count() > 1) {
             String[] credentials = data.split(" ");
             if (isCorrectName(credentials[0], credentials[1]) && isCorrectEmail(credentials[credentials.length - 1])) {
+                addStudent(credentials[credentials.length - 1]);
                 System.out.println(Messages.ADD_SUCCESSFUL.getMessage());
                 return true;
             }
@@ -32,10 +43,28 @@ public class Student {
 
     static boolean isCorrectEmail(String email) {
         String emailRegex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+\\.+[a-zA-Z0-9.-]+$";
-        if (email.matches(emailRegex)) {
+        if (email.matches(emailRegex) && isEmailUnique(email)) {
             return true;
+        } else if (email.matches(emailRegex) && !isEmailUnique(email)) {
+            System.out.println(Messages.EMAIL_EXISTS.getMessage());
+            return false;
         }
         System.out.printf(Messages.ADD_ERROR.getMessage(), "email");
         return false;
+    }
+
+    static boolean isEmailUnique(String email) {
+        if (!students.isEmpty() && students.containsValue(email)) {
+            return false;
+        } else if (students.isEmpty()) {
+            return true;
+        } else {
+            return true;
+        }
+    }
+
+    static void addStudent(String email) {
+        students.put(String.valueOf(email.hashCode()), email);
+        //students.entrySet().forEach(System.out::println);
     }
 }
