787a9588eb2f6dbab9525596565ed2d6753e7c9f
Olga Fomenkova <olga.fomenkova@pprcard.ru>
2023-11-13 18:26:27 +0300

    Unit tests for the 2nd stage and partly 3rd stage

---
+++ Learning Progress Tracker (Java)/task/test/tracker/CredentialsTest.java
@@ -0,0 +1,63 @@
+package tracker;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CredentialsTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Ann Doe ann@doe.com", "Jean-Claude O'Neill j.cld@m.work.com", "Robert Jemison Van de Graaff robert.g@xyz.to"})
+    void goodCredentials(String input) {
+        Student student = new Student(input);
+        assertEquals(input, student.getFirstName() + " " + student.getLastName() + " " + student.getEmail());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "help", " "})
+    void badCredentials(String input) {
+        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> new Student(input));
+        assertEquals(String.format(Messages.ADD_ERROR.getMessage(), "credentials"), thrown.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"A Doe ann@doe.com", "J-0 Claude j.cld@m.work.com", "123 Bob realdeal@yeah.yeah"})
+    void credentialsWithBadFirstName(String input) {
+        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> new Student(input));
+        assertEquals(String.format(Messages.ADD_ERROR.getMessage(), "first name"), thrown.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Ann D ann@doe.com", "Jean-Claude B2 j.cld@m.work.com", "Bob - realdeal@yeah.yeah"})
+    void credentialsWithBadLastName(String input) {
+        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> new Student(input));
+        assertEquals(String.format(Messages.ADD_ERROR.getMessage(), "last name"), thrown.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Ann Doe doe @doe.com", "Jean-Claude O'Neill j*c!l&dm.work.com", "John Ronald Reuel Tolkien writer.com"})
+    void credentialsWithBadEmail(String input) {
+        Throwable thrown = assertThrows(IllegalArgumentException.class, () -> new Student(input));
+        assertEquals(String.format(Messages.ADD_ERROR.getMessage(), "email"), thrown.getMessage());
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({"'Ann Doe user@doe.com', 'John Doe user@doe.com'"})
+    void credentialsWithDuplicateEmail(String firstInput, String secondInput) {
+        Tracker tracker = new Tracker();
+        assertTrue(tracker.addStudent(firstInput));
+        assertFalse(tracker.addStudent(secondInput));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Ann Doe ann@doe.com", "Jean-Claude O'Neill j.cld@m.work.com", "Robert Jemison Van de Graaff robert.g@xyz.to"})
+    void checkStudentIdAdded(String input) {
+        Tracker tracker = new Tracker();
+        assertTrue(tracker.addStudent(input));
+    }
+
+
+}
